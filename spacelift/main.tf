713e39dde01189d46b2d1a226570d070df196882
Flavius <flaviuscdinu93@gmail.com>
2024-05-17 16:55:12 +0300

    Added spacelift configuration

---
+++ spacelift/main.tf
@@ -0,0 +1,50 @@
+provider "spacelift" {}
+
+resource "spacelift_stack" "k8s-cluster" {
+  branch            = "main"
+  description       = "Provisions a Kubernetes cluster"
+  name              = "Terraform Kubernetes Cluster"
+  project_root      = "terraform"
+  repository        = "eks-argo-terraform"
+  terraform_version = "1.5.7"
+
+  labels = ["terraform-argocd"]
+}
+
+resource "spacelift_stack" "argocd" {
+  kubernetes {
+    namespace = "default"
+  }
+  branch       = "main"
+  description  = "Deploys an ArgoCD application"
+  name         = "ArgoCD application"
+  project_root = "argocd/config"
+  repository   = "eks-argo-terraform"
+  labels       = ["terraform-argocd"]
+  before_init  = ["$AWS_LOGIN"]
+}
+
+resource "spacelift_aws_integration_attachment" "k8s-cluster" {
+  integration_id = var.integration_id
+  stack_id       = spacelift_stack.k8s-cluster.id
+  read           = true
+  write          = true
+}
+
+resource "spacelift_aws_integration_attachment" "argocd" {
+  integration_id = var.integration_id
+  stack_id       = spacelift_stack.k8s-cluster.id
+  read           = true
+  write          = true
+}
+
+resource "spacelift_stack_dependency" "cluster-argo" {
+  stack_id            = spacelift_stack.argo.id
+  depends_on_stack_id = spacelift_stack.k8s-cluster.id
+}
+
+resource "spacelift_stack_dependency_reference" "output" {
+  stack_dependency_id = spacelift_stack_dependency.cluster-argo.id
+  output_name         = "eks_connect"
+  input_name          = "AWS_LOGIN"
+}
