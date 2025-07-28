870353aa2679bb1a950e6415d76edd5b694e62e2
Flavius Dinu <57911064+flavius-dinu@users.noreply.github.com>
2025-07-28 10:06:08 +0300

    Update argocd.tf

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -5,7 +5,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = aws_eks_cluster.main.endpoint
     token                  = data.aws_eks_cluster_auth.main.token
     cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
